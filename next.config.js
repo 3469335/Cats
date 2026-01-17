d2776b2ceeb3d19933423f15f93f1db5b274879e
3469335 <3469335@gmail.com>
2026-01-17 16:45:13 +0300

    V19

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Отключаем ESLint во время сборки на Vercel (проверка типов все равно выполняется)
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Продолжаем проверку типов, но не блокируем сборку при ошибках
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
